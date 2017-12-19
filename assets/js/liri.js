04e9172255288616201487f13bd5eefab0a6491f
Ovie <you@example.com>
2017-12-19 00:48:58 -0500

    appends results to log.txt

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -13,15 +13,15 @@ var param1 = process.argv[3];
 var param2 = process.argv[4];
 var movieName = "";
 
-function writeLog(output){
-	var fs = require("fs");
-	fs.appendFile("log.txt",output, function(err){
-		if(err){
-			console.log(err);
-		}
-	})
+function writeLog(output) {
+    var fs = require("fs");
+    fs.appendFile("../log.txt", output + "\n", function(err) {
+        if (err) {
+            console.log(err);
+        }
+    })
 }
-writeLog("working");
+
 
 function getTwitter() {
     //request info from twitter
@@ -33,14 +33,14 @@ function getTwitter() {
         access_token_key: twitterAccessTokenKey,
         access_token_secret: twitterAccessTokenSecret
     });
-
+    writeLog("Twitter Search:");
     client.get('statuses/user_timeline', function(error, tweets, response) {
         if (error) {
             console.log(error);
         }
         for (var i = 0; i < tweets.length; i++) {
             console.log("Tweet: " + tweets[i].text + "; written on " + tweets[i].created_at);
-        	writeLog("Tweet: " + tweets[i].text + "; written on " + tweets[i].created_at);
+            writeLog("Tweet: " + tweets[i].text + "; written on " + tweets[i].created_at);
         }
     });
 }
@@ -53,31 +53,33 @@ function getSpotify() {
         id: spotifyClientId,
         secret: spotifyClientSecret
     });
-
+    writeLog("Spotify Search:");
     spotify.search({ type: 'track', query: param1 }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
-        }
-        else {
+        } else {
             //artist
             console.log("Artist: " + data.tracks.items[0].artists[0].name);
             writeLog("Artist: " + data.tracks.items[0].artists[0].name);
             //song name
             console.log("Song name: " + data.tracks.items[0].name);
+            writeLog("Song name: " + data.tracks.items[0].name);
             //preview link
             console.log("Preview this song: " + data.tracks.items[0].external_urls.spotify);
+            writeLog("Preview this song: " + data.tracks.items[0].external_urls.spotify);
             //album name
             console.log("Album: " + data.tracks.items[0].album.name);
-
+            writeLog("Album: " + data.tracks.items[0].album.name);
         }
     });
 }
 
 function getOmdb() {
+
     //request info from omdb
     var request = require("request");
     var nodeArgs = process.argv;
-    
+
     for (var i = 3; i < nodeArgs.length; i++) {
         if (i > 3 && i < nodeArgs.length) {
             movieName = movieName + "+" + nodeArgs[i];
@@ -85,73 +87,80 @@ function getOmdb() {
             movieName += nodeArgs[i];
         }
     }
+    writeLog("omdb Search:");
     request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
         if (error) {
             console.log(error);
         } else if (!error && response.statusCode === 200) {
             bodyObj = JSON.parse(body);
             console.log("Title: " + bodyObj.Title);
+            writeLog("Title: " + bodyObj.Title);
             console.log("Year: " + bodyObj.Year);
+            writeLog("Year: " + bodyObj.Year);
             for (var i = 0; i < 2; i++) {
                 if (bodyObj.Ratings[i]) {
                     console.log(bodyObj.Ratings[i].Source + " Rating: " + bodyObj.Ratings[i].Value);
+                    writeLog(bodyObj.Ratings[i].Source + " Rating: " + bodyObj.Ratings[i].Value);
                 } else {
                     console.log("No Ratings Available");
+                    writelog("No Ratings Available");
                 }
             }
             console.log("Produced in: " + bodyObj.Country);
             console.log("Language: " + bodyObj.Language);
             console.log("Plot: " + bodyObj.Plot);
-            console.log("Starring: " + bodyObj.Actors)
+            console.log("Starring: " + bodyObj.Actors);
+            writeLog("Produced in: " + bodyObj.Country);
+            writeLog("Language: " + bodyObj.Language);
+            writeLog("Plot: " + bodyObj.Plot);
+            writeLog("Starring: " + bodyObj.Actors)
         }
     })
 }
 
 
-function liri(){
-//twitter
-if (operand === 'my-tweets') {
-    getTwitter();
+function liri() {
+    //twitter
+    if (operand === 'my-tweets') {
+        getTwitter();
 
-} else if (operand === 'spotify-this-song') {
-	if(!param1){
-		param1="The Sign";
-		console.log(param1);
-		getSpotify();
-	}
-	else{
-    getSpotify();
-}
+    } else if (operand === 'spotify-this-song') {
+        if (!param1) {
+            param1 = "The Sign";
+            console.log(param1);
+            getSpotify();
+        } else {
+            getSpotify();
+        }
 
-} else if (operand === 'movie-this') {
-	if(!param1){
-		movieName="Mr Nobody";
-		getOmdb();
-	}
-	else{
-	getOmdb();
-}
-} 
+    } else if (operand === 'movie-this') {
+        if (!param1) {
+            movieName = "Mr Nobody";
+            getOmdb();
+        } else {
+            getOmdb();
+        }
+    }
 
-}//ends liri function
+} //ends liri function
 liri();
 
 if (operand === 'do-what-it-says') {
-   function readRandom(){
-	//read random.txt
-    var fs = require("fs");
-    fs.readFile("../random.txt", "utf8", function(error, data) {
-        if (error) {
-            console.log(error)
-        } else {
-            var splitData = data.split(",");
-            console.log(splitData);
-            operand = splitData[0];
-            param1 = splitData[1];
-            liri();
-        }
-    })
-} 
+    function readRandom() {
+        //read random.txt
+        var fs = require("fs");
+        fs.readFile("../random.txt", "utf8", function(error, data) {
+            if (error) {
+                console.log(error)
+            } else {
+                var splitData = data.split(",");
+                console.log(splitData);
+                operand = splitData[0];
+                param1 = splitData[1];
+                liri();
+            }
+        })
+    }
     readRandom();
-  
+
 }
